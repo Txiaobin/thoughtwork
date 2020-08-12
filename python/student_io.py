f47aad911d53d3452951c77f38fa77746c2a7125
xiaobin tian <xiaobin9652@163.com>
2020-08-12 17:28:38 +0800

    Update student_io.py

--- python/student_io.py
+++ python/student_io.py
@@ -1,8 +1,7 @@
 import pandas as pd
-'''
+
 def convert_file_format(input_file_path: str, output_file_path: str,
                         input_format: str = 'csv', output_format: str = 'json'):
+    f = pd.read_csv('student.csv')
+    f.to_json('newstudent.json')
 
-'''
-f = pd.read_csv('student.csv')
-f.to_json('newstudent.json')
