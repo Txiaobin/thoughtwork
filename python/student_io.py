f767c5678709d93630dea4159832d648921530e2
xiaobin tian <xiaobin9652@163.com>
2020-08-14 15:47:49 +0800

    udate student_io.py

--- python/student_io.py
+++ python/student_io.py
@@ -1,7 +1,14 @@
-import pandas as pd
+import csv
+import json
 
 def convert_file_format(input_file_path: str, output_file_path: str,
                         input_format: str = 'csv', output_format: str = 'json'):
-    f = pd.read_csv('student.csv')
-    f.to_json('newstudent.json')
+
+    csvfile = open(input_file_path,'r', encoding='utf-8')
+    jsonfile = open(output_file_path, 'w',encoding='utf-8')
+    fieldnames = ('name','age','gender','class','score')
+    
+    reader = csv.DictReader( csvfile, fieldnames)
+    out = json.dumps( [ row for row in reader ] ,ensure_ascii=False)
+    jsonfile.write(out)
 
--- python/student_io_test.py
+++ python/student_io_test.py
@@ -6,13 +6,13 @@ from student_io import convert_file_format
 
 class StudentIOTest(unittest.TestCase):
     def test_convert_file_format(self):
-        convert_file_format('./student.csv', './student.json', 'csv', 'json')
+        convert_file_format('student.csv', 'student.json', 'csv', 'json')
 
-        with open('./student.json', 'r', encoding='utf8') as f:
+        with open('student.json', 'r', encoding='utf8') as f:
             students = json.load(f)
 
-        self.assertDictEqual(students[0], {
-            'name': '张三', 'age': 18, 'gender': '男', 'class': '高三三班', 'score': 98
+        self.assertDictEqual(students[1], {
+            'name': '张三', 'age': '18', 'gender': '男', 'class': '高三三班', 'score': '98'
         })
 
 
